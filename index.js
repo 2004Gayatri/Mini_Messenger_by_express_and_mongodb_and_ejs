34c165b33e17e74b724c24cab1b864155119bbfa
gayatri2004 <gayatrisanap2572004@gmail.com>
2025-09-30 15:52:11 +0530

    🚀 First ping: Mini Messenger app setup with Express, MongoDB, and EJS

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+app.set("views",path.join(__dirname, "views"));
+app.set("view engine","ejs");
+
+main().then(()=>{console.log("Connection is successful")}).catch((err)=>{console.log(err)});
+async function main(){
+   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chat1 = new Chat({
+    from : "Gayatri",
+    to : "Priya",
+    msg : "Send me my project that i shared with you ",
+    created_at : new Date()
+
+});
+chat1.save().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});
+
+
+
+
+app.get("/" , (req,res)=>{
+    res.send("Root is working !!");
+})
+
+
+
+app.listen(8080,()=>{
+    console.log("Server is listening on port 8080 ! ");
+})
