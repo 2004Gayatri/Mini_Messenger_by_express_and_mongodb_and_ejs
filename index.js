dcd7f9811864c62adf37387df4b1d622e981cf65
gayatri2004 <gayatrisanap2572004@gmail.com>
2025-10-03 12:34:28 +0530

    Finally completed all operations on mini-chat project

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+
+const methodOverride = require("method-override");
+
 //this is used to parse useful data from api body
 app.use(express.urlencoded({extended:true}));
 
 app.set("views",path.join(__dirname, "views"));
 app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname , "public")))
+app.use(express.static(path.join(__dirname , "public")));
+app.use(methodOverride("_method"));
 
 
 main().then(()=>{console.log("Connection is successful")}).catch((err)=>{console.log(err)});
@@ -38,8 +42,35 @@ app.post("/chats",(req,res)=>{
   });
   newChat.save().then((res)=>{console.log("Chat is saved")}).catch((err)=>{console.log(err)});
   res.redirect("/chats");
+});
+
+// Edit Route
+app.get("/chats/:id/edit" , async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
 })
 
+//UPDATE Route
+app.put("/chats/:id",async(req,res)=>{
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    try{
+      let updatedChat = await Chat.findByIdAndUpdate(id,{msg : newMsg}, {runValidators:true , new : true});
+    console.log(updatedChat); 
+    res.redirect("/chats");
+    }catch(err){
+       res.send("Error updating chat");
+    }
+
+});
+
+// delete Route
+app.delete("/chats/:id",async(req,res)=>{
+  let {id} = req.params;
+  await Chat.findByIdAndDelete(id);
+  res.redirect("/chats");
+})
 
 app.get("/" , (req,res)=>{
     res.send("Root is working !!");
